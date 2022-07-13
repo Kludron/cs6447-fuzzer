be4955745d3746ac3ee4f3071e94b9ca6a7c599f
Jin Shan Ho <z5230926@ad.unsw.edu.au>
2022-07-14 07:45:52 +1000

    able to detect csv, json, xml

--- fuzzer/src/fuzzer.py
+++ fuzzer/src/fuzzer.py
@@ -1,8 +1,84 @@
 # COMP6447 Fuzzer
-"""
+import sys
+import json
+import xml.etree.ElementTree as ElementTree
+from pwn import *
 
-"""
+TYPE_FAIL = -1
+TYPE_CSV = 0
+TYPE_JSON = 1
+TYPE_JPG = 2
+TYPE_XML = 3
+TYPE_PLAINTEXT = 4
+
+# Generic Fuzzer Class
+class Fuzz():
+    def __init__(self, input):
+        self.input = input
+    def checkType(self):
+        return False
+
+# CSV Fuzzer
+class CSV_Fuzz(Fuzz):
+    def __init__(self, input):
+        super().__init__(input)
+    def checkType(self):
+        self.lines = self.input.split("\n")
+        first_row_commas = self.lines[0].count(",")
+        
+        for line in self.lines:
+            if line.count(",") != first_row_commas or first_row_commas == 0:
+                return False
+        return True
+
+# JSON Fuzzer
+class JSON_Fuzz(Fuzz):
+    def __init__(self, input):
+        super().__init__(input)
+    def checkType(self):
+        try:
+            json.loads(self.input)
+            return True
+        except ValueError:
+            return False
+
+# XML Fuzzer
+class XML_Fuzz(Fuzz):
+    def __init__(self, input):  
+        super().__init__(input)
+    def checkType(self):
+        try:
+            ElementTree.fromstring(self.input)
+            return True
+        except ElementTree.ParseError:
+            return False
+def checkType(filename):
+    try:
+        fp = open(filename, 'r')
+        inputTxt = fp.read().strip()
+        # print(inputTxt)
+        if (CSV_Fuzz(inputTxt).checkType()):
+            return TYPE_CSV
+        if (JSON_Fuzz(inputTxt).checkType()):
+            return TYPE_JSON
+        if (XML_Fuzz(inputTxt).checkType()):
+            return TYPE_XML
+        else:
+            return TYPE_FAIL
+    except IOError:
+        return TYPE_FAIL
 
 
 if __name__ == '__main__':
-    print("Hello there!")
+    print("Sample input: ", sys.argv[1])
+    
+    type = checkType(sys.argv[1])
+            
+    if type == TYPE_FAIL:
+        print("Failed to open file/detect input type")
+    elif type == TYPE_CSV:
+        print("Detected CSV")
+    elif type == TYPE_JSON:
+        print("Detected JSON")
+    elif type == TYPE_XML:
+        print("Detected XML")
