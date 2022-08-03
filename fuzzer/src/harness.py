c995d005c129b94fcf45481c4decb892abf43a27
Your Name <you@example.com>
2022-08-04 00:20:35 +1000

    Detecting crash types, fixed closure of threads

--- fuzzer/src/harness.py
+++ fuzzer/src/harness.py
@@ -12,10 +12,42 @@ import signal
 
 from utils import Fuzz
 
+#BINARY RETURN CODES
+SIGHUP = -1
+SIGINT = -2
+SIGQUIT = -3
+SIGILL = -4
+SIGTRAP = -5
+SIGABRT = -6
+SIGBUS = -7
+SIGFPE = -8
+SIGKILL = -9
+SIGUSR1 = -10
+SIGSEGV = -11
+SIGUSR2 = -12
+SIGPIPE = -13
+SIGALRM = -14
+SIGTERM = -15
+SIGSTKFLT = -16
+SIGCHLD = -17
+SIGCONT = -18
+SIGSTOP = -19
+SIGTSTP = -20
+SIGTTIN = -21
+SIGTTOU = -22
+SIGURG = -23
+SIGXCPU = -24
+SIGXFSZ = -25
+SIGVTALRM = -26
+SIGPROF = -27
+SIGWINCH = -28
+SIGIO = -29
+SIGPWR = -30
+SIGSYS = -31
+
 class Harness():
 
     def __init__(self, program: str, seed: str, fuzzer: Fuzz) -> None:
-
         self.program = program
         self.seed = seed
         self.fuzzer = fuzzer
@@ -37,13 +69,14 @@ class Harness():
         self.s_semaphore = Semaphore()
 
         self.success = False
+        self.crash_type = None
         self.success_semaphore = Semaphore()
 
         self.out_semaphore = Semaphore()
         self.outfile = open('bad.txt', 'w')
 
-    def start(self) -> None:
 
+    def start(self) -> None:
         # Check if the harness has already been started
         self.s_semaphore.acquire()
         if self.isStarted: return
@@ -69,8 +102,6 @@ class Harness():
                 thread.start()
 
 
-
-
     def test(self):
         t = current_thread()
         t.alive = True
@@ -84,32 +115,90 @@ class Harness():
                     self.c_semaphore.acquire()
                     self.counter += 1
                     self.c_semaphore.release()
-                    subprocess.run(self.program, input=fuzzInput, check=True, stdout=PIPE, text=True)
+                    subprocess.run(self.program, input=fuzzInput, check=True, stdout=PIPE, text=True, preexec_fn = lambda: signal.signal(signal.SIGINT, signal.SIG_IGN))
                     self.LOGFILE.write(fuzzInput+'\n...\n')
                 except subprocess.CalledProcessError as e:
-                    if e.returncode != -2:
-                        self.out_semaphore.acquire()
-                        self.outfile.write(fuzzInput + '\n')
-                        self.success_semaphore.acquire()
-                        self.success = True
-                        self.success_semaphore.release()
-                else:
-                    pass
+                    self.out_semaphore.acquire()
+                    self.outfile.write(fuzzInput + '\n')
+                    self.success_semaphore.acquire()
+                    self.success = True
+                    if e.returncode == SIGHUP:
+                        self.crash_type = "SIGHUP"
+                    elif e.returncode == SIGINT:
+                        self.crash_type = "SIGINT"
+                    elif e.returncode == SIGQUIT:
+                        self.crash_type = "SIGQUIT"
+                    elif e.returncode == SIGILL:
+                        self.crash_type = "SIGILL"
+                    elif e.returncode == SIGTRAP:
+                        self.crash_type = "SIGTRAP"
+                    elif e.returncode == SIGABRT:
+                        self.crash_type = "SIGABRT"
+                    elif e.returncode == SIGBUS:
+                        self.crash_type = "SIGBUS"
+                    elif e.returncode == SIGFPE:
+                        self.crash_type = "SIGFPE"
+                    elif e.returncode == SIGKILL:
+                        self.crash_type = "SIGKILL"
+                    elif e.returncode == SIGUSR1:
+                        self.crash_type = "SIGUSR1"
+                    elif e.returncode == SIGSEGV:
+                        self.crash_type = "SIGSEGV"
+                    elif e.returncode == SIGUSR2:
+                        self.crash_type = "SIGUSR2"
+                    elif e.returncode == SIGPIPE:
+                        self.crash_type = "SIGPIPE"
+                    elif e.returncode == SIGALRM:
+                        self.crash_type = "SIGALRM"
+                    elif e.returncode == SIGTERM:
+                        self.crash_type = "SIGTERM"
+                    elif e.returncode == SIGSTKFLT:
+                        self.crash_type = "SIGSTKFLT"
+                    elif e.returncode == SIGCHLD:
+                        self.crash_type = "SIGCHLD"
+                    elif e.returncode == SIGCONT:
+                        self.crash_type = "SIGCONT"
+                    elif e.returncode == SIGSTOP:
+                        self.crash_type = "SIGSTOP"
+                    elif e.returncode == SIGTSTP:
+                        self.crash_type = "SIGTSTP"
+                    elif e.returncode == SIGTTIN:
+                        self.crash_type = "SIGTTIN"
+                    elif e.returncode == SIGTTOU:
+                        self.crash_type = "SIGTTOU"
+                    elif e.returncode == SIGURG:
+                        self.crash_type = "SIGURG"
+                    elif e.returncode == SIGXCPU:
+                        self.crash_type = "SIGXCPU"
+                    elif e.returncode == SIGXFSZ:
+                        self.crash_type = "SIGXFSZ"
+                    elif e.returncode == SIGVTALRM:
+                        self.crash_type = "SIGVTALRM"
+                    elif e.returncode == SIGPROF:
+                        self.crash_type = "SIGPROF"
+                    elif e.returncode == SIGWINCH:
+                        self.crash_type = "SIGWINCH"
+                    elif e.returncode == SIGIO:
+                        self.crash_type = "SIGIO"
+                    elif e.returncode == SIGPWR:
+                        self.crash_type = "SIGPWR"
+                    elif e.returncode == SIGSYS:
+                        self.crash_type = "SIGSYS"
+                    self.success_semaphore.release() 
+        sys.exit(0)
 
         
-
-
     def fuzz(self) -> None:
         """
         Fuzzer function generates mutations and places mutations on to the queue
         """
         t = current_thread()
         t.alive = True
-        for _ in range(self.MAX_TESTS):
+        for _ in range(self.MAX_TESTS):     
             if t.alive == True:
                 '''
-                # Legit input to valid that no segfault occurs initially before fuzzer output used
-                for i in range(10000):
+                # Legit input to validate that no segfault occurs initially before fuzzer output used
+                for i in range(100000):
                     try:
                         self.queue.put(r'{"len": 12, "input": "AAAABBBBCCCC", "more_data": ["a", "bb"]}')                    
                     except Full:
@@ -122,17 +211,23 @@ class Harness():
                 except Full:
                     pass
             else:
-                sys.exit(0)
+                break   
+        sys.exit(0)
 
 
     def finish(self):
         print('Closing threads...')
+        total = len(enumerate()) - 1
+        counter = 1
         for t in enumerate():
-            t.alive = False
-        time.sleep(3)
+            if t is not main_thread():
+                print(f"Closing thread {counter} of {total}....", end="")
+                t.alive = False
+                t.join(1)
+                print(f"closed")
+                counter += 1
         self.LOGFILE.close()
         self.outfile.close()
-        print('Output saved to bad.txt')  
 
 
     def monitor(self, refresh_time=2) -> None:
@@ -186,8 +281,9 @@ class Harness():
                     print("Press Ctrl + C to exit.")
 
                 elif self.success == True:
-                    print('Success!')
+                    print(f"Success! Crash type: {self.crash_type}")
                     self.finish()
+                    print('Output saved to bad.txt')  
                     return
 
                 prev_time = curr_time
